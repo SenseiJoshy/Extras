ef05af68a900cdb251ed4f3454515e2bf6b8b578
Joshy <88342946+SenseiJoshy@users.noreply.github.com>
2021-12-04 20:32:30 -0400

    Create Legend Piece Infi Beli Level Gems.lua

---
+++ Legend Piece Infi Beli Level Gems.lua
@@ -0,0 +1,3 @@
+game:GetService("ReplicatedStorage").Remotes.Stats:FireServer(game:GetService("Players").LocalPlayer.Data.Level,99999) --Make sure its  x10 more than the level u are
+game:GetService("ReplicatedStorage").Remotes.Stats:FireServer(game:GetService("Players").LocalPlayer.Data.Beli,99999) --Make sure its  x10 more than the beli u have
+game:GetService("ReplicatedStorage").Remotes.Stats:FireServer(game:GetService("Players").LocalPlayer.Data.Gem,99999) --Make sure its  x10 more than the gems u have
