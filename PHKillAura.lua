708dfccee2bf83aa4d7f10ddb97ca1675db2bc76
Joshy <88342946+SenseiJoshy@users.noreply.github.com>
2021-12-11 16:53:28 -0400

    Create PHKillAura.lua

---
+++ PHKillAura.lua
@@ -0,0 +1,10 @@
+getgenv().killaura = not getgenv().killaura
+while getgenv().killaura do wait(0.1)
+    local args = {
+       [1] = "Handle_Combat",
+       [2] = game:GetService("Players").LocalPlayer.Character,
+       [3] = 1
+    }
+    
+    game:GetService("ReplicatedStorage").Remotes.To_Server.Handle_Initiate_S:FireServer(unpack(args))
+end
