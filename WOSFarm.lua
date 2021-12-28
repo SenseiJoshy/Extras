561db29df42c5a52e337dbb0ba9c57fa6c32dd19
Joshy <88342946+SenseiJoshy@users.noreply.github.com>
2021-12-28 16:23:39 -0400

    Update WOSFarm.lua

--- WOSFarm.lua
+++ WOSFarm.lua
@@ -1,17 +1,19 @@
 --[[
   Really trying to steal a trash script i spent 30 seconds on LMAOOOO
-  Discord - https://sensei-joshy.xyz/discord
+  Discord - https://senseijoshy.com/Discord
 ]]--
-getgenv().Farm = True
+getgenv().ItemFarm = true --Change to false to turn off sped
 
-while getgenv().Farm do wait()
-    pcall(function()
-        for _,v in pairs(game.Workspace:GetDescendants()) do
-            if v:IsA("TouchTransmitter") then
-                firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, v.Parent, 0) --0 is touch
-                wait()
-                firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, v.Parent, 1) -- 1 is untouch
-            end
-        end
-    end)
-end
+game:GetService("RunService").Stepped:connect(function()
+  pcall(function()
+      if ItemFarm then
+          for _,v in pairs(game.Workspace:GetDescendants()) do
+           if v:IsA("TouchTransmitter") then
+               firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, v.Parent, 0)
+               wait()
+               firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, v.Parent, 1)
+               end
+           end
+      end
+  end)
+end)
