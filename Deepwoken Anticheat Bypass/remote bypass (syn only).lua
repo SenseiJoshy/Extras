55df622d82b9c5cc2697caa1c20ab68bfb36b072
Joshy <88342946+SenseiJoshy@users.noreply.github.com>
2022-01-07 17:03:03 -0400

    Create remote bypass (syn only).lua

---
+++ Deepwoken Anticheat Bypass/remote bypass (syn only).lua
@@ -0,0 +1,9 @@
+local ui_manager = game:GetService("Players").LocalPlayer.PlayerScripts.UIManager;
+local ui_manager_get_remote = getupvalue(getconnections(game:GetService("TextService").ChildAdded)[1].Function, 2);
+
+local function_key = getconstant(ui_manager_get_remote, 1);
+local get_key_function = getupvalue(ui_manager_get_remote, 1)[1];
+
+local function get_remote(remote_name)
+   return syn.secure_call(get_key_function, ui_manager, remote_name, function_key);
+end;
