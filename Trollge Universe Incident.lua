b0b98910dc83ee18913a26068766c9a389d956ef
Joshy <88342946+SenseiJoshy@users.noreply.github.com>
2022-01-08 20:55:19 -0400

    Create Trollge Universe Incident.lua

---
+++ Trollge Universe Incident.lua
@@ -0,0 +1,16 @@
+[[--
+  getgenv().ChestFarm = true --Change to false to turn off sped 
+--]]
+
+spawn(function()
+    while ChestFarm do wait()
+        pcall(function()
+            for i,v in pairs(game:GetService("Workspace").chests:GetDescendants()) do
+                if v:IsA("Part") then
+                    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.CFrame
+                    fireproximityprompt(v.ProximityPrompt)
+                end
+            end
+        end)
+    end
+end)
