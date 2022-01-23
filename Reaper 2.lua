4d06a1d4fee06484050ae574625c4ecb1350d415
Sensei Joshy#1060 <88457646+SenseiiJoshy@users.noreply.github.com>
2022-01-23 13:26:30 -0400

    Update Reaper 2.lua

--- Reaper 2.lua
+++ Reaper 2.lua
@@ -35,6 +35,17 @@ for i,v in pairs(game:GetService("Workspace").NPCs:GetChildren()) do
     end);
 end;
 
+game:GetService("RunService").Stepped:Connect(function()
+    if getgenv().Autofarm then
+        for i,v in pairs(game.Players.LocalPlayer.Character:GetDescendants()) do
+            if v:IsA("BasePart") and v.CanCollide == true then
+                v.CanCollide = false
+                game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity = Vector3.new(0,0,0)
+            end;
+        end;
+    end;
+end);
+
 local Material = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kinlei/MaterialLua/master/Module.lua"))()
 
 	local UI = Material.Load({
